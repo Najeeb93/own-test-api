d4bce9c31c0453d6ce967eb910190ef175d62cae
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-13 15:18:30 +0500

    update

--- src/app/(root)/courses/page.js
+++ src/app/(root)/courses/page.js
@@ -10,9 +10,13 @@ const Course = async () => {
             <h1 className="text-3xl text-fuchsia-800 font-bold underline bg-fuchsia-400 p-3 text-center">Courses</h1>
             <div className="grid grid-col-4 container mx-auto gap-4 mt-5 ">
                 {
-                    course.course.map((date) => (
-                        <div>
-                            
+                    course.course.map((data) => (
+                        <div key={data.id} className="bg-fuchsia-100 border-2 border-fuchsia-600 rounded p-3 space-y-2">
+                             <h1><span className=' font-bold'>Title: </span> {data.title}</h1>
+                            <h1><span className=' font-bold'>Duration: </span> {data.duration}</h1>
+                            <h1><span className=' font-bold'>Timing: </span> {data.timing}</h1>
+                            <h1><span className=' font-bold'>Teacher: </span> {data.teacher}</h1>
+
                         </div>
                     ))
                 }
