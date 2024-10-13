f5aa9948d00321ab2d41fa8722b3943fe876cda3
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-13 14:31:45 +0500

    add to commit

--- src/app/api/crouses/route.js
+++ src/app/api/crouses/route.js
@@ -1,3 +1,16 @@
 const course = [
-    
+    {
+        id: 1,
+        title: "Modern Web Development",
+        duration: "3-4 Month",
+        timing: "9-11 am",
+        teacher: "Sir Bilal"
+    },
+    {
+        id: 2,
+        title: "Modern Web Development",
+        duration: "3-4 Month",
+        timing: "9-11 am",
+        teacher: "Sir Ghous"
+    },
 ]
