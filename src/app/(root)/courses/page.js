90f373057093709fc87de7b51de423632fd90bcb
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-10-13 15:06:21 +0500

    update page

--- src/app/(root)/courses/page.js
+++ src/app/(root)/courses/page.js
@@ -1,4 +1,11 @@
 
 const Course = async () => {
     let course = await fetch('http://localhost:3000/api/course')
+    course = await course.json();
+    console.log(course);
+
+    return (
+        <h1>Courses</h1>
+    )
+    
 }
